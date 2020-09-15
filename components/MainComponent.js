fac9488fb156c92363093c445c21f8e7d615d6a9
Bonnie <mylilangls@gmail.com>
2020-09-15 14:33:30 -0700

    Test Commit

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
+import About from './AboutComponent';
+import Contact from './ContactComponent';
 import { View, Platform } from 'react-native';
-import { createStackNavigator } from 'react-navigation';
+import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
 
 const DirectoryNavigator = createStackNavigator(
     {
@@ -23,6 +26,72 @@ const DirectoryNavigator = createStackNavigator(
     }
 );
 
+const AboutNavigator= createStackNavigator(
+    {
+        Home: { screen: About }
+       
+    },
+    {
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: '#563700'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff',
+            }
+        }
+    }
+);
+
+const HomeNavigator= createStackNavigator(
+    {
+        Home: { screen: Home }
+       
+    },
+    {
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: '#563700'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff',
+            }
+        }
+    }
+);
+
+const ContactNavigator= createStackNavigator(
+    {
+        Home: { screen: Contact }
+       
+    },
+    {
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: '#563700'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff',
+            }
+        }
+    }
+);
+
+const MainNavigator = createDrawerNavigator(
+    {
+        Home: { screen: HomeNavigator },
+        Directory: { screen: DirectoryNavigator },
+        About: { screen: AboutNavigator },
+        Contact: { screen: ContactNavigator }
+    },
+    {
+        drawerBackgroundColor: '#CEC8FF'
+    }
+);
+
 class Main extends Component {
     render() {
         return (
@@ -30,7 +99,7 @@ class Main extends Component {
                 flex: 1,
                 paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight
                 }}>
-                    <DirectoryNavigator />
+                    <MainNavigator />
             </View>
         );        
     }
