9be7a862760ff4027fc71f6b8113b31bcd2b7daa
Bonnie <mylilangls@gmail.com>
2020-09-19 10:14:06 -0700

    Activity Indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,6 +3,7 @@ import { Text, ScrollView, FlatList } from 'react-native';
 import { ListItem, Card } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -10,20 +11,23 @@ const mapStateToProps = state => {
     };
 };
 
-const Mission = () => {
+function Mission() {
     return (
-        <Text>
-            We present a curated database of the best campsites 
-            in the vast woods and backcountry of the World Wide Web Wilderness. 
-            We increase access to adventure for the public while promoting safe 
-            and respectful use of resources. The expert wilderness trekkers on our 
-            staff personally verify each campsite to make sure that they are up to our standards.
-            We also present a platform for campers to share reviews on campsites 
-            they have visited with each other.
-        </Text>  
-        );
-    }
-    class About extends Component {
+        <Card title='Our Mission'>
+            <Text style={{margin: 10}}>
+                We present a curated database of the best campsites 
+                in the vast woods and backcountry of the World Wide Web Wilderness. 
+                We increase access to adventure for the public while promoting safe 
+                and respectful use of resources. The expert wilderness trekkers on our 
+                staff personally verify each campsite to make sure that they are up to our standards.
+                We also present a platform for campers to share reviews on campsites 
+                they have visited with each other.
+            </Text>  
+        </Card>
+    );
+}
+    
+class About extends Component {
     
     static navigationOptions = {
         title: 'About Us'
@@ -37,21 +41,41 @@ render() {
                 leftAvatar={{ source: {uri: baseUrl + item.image}}}
             /> 
         );
-    };     
+    };
+    
+    if (this.props.partners.isLoading) {
+        return (
+            <ScrollView>
+                <Mission />
+                    <Card title="Community Partners">
+                        <Loading />
+                    </Card>
+            </ScrollView> 
+        );
+    }
+
+    if (this.props.partners.errMess) {
+        return (
+            <ScrollView>
+                <Mission />
+                    <Card title="Community Partners">
+                        <Text>{this.props.partners.errMess}</Text>
+                    </Card>
+            </ScrollView> 
+        )
+    }
         return ( 
             <ScrollView>
-                <Card title="Our Mission">
-                    <Mission />
-                </Card>
-                <Card title="Community Partners">
-                    <FlatList
-                        data={this.props.partners.partners}
-                        renderItem={renderPartner}
-                        keyExtractor={item => item.id.toString()}
-                    />
-                </Card>
+                <Mission />
+                    <Card title="Community Partners">
+                        <FlatList
+                            data={this.props.partners.partners}
+                            renderItem={renderPartner}
+                            keyExtractor={item => item.id.toString()}
+                        />
+                    </Card>
             </ScrollView> 
-            );
-        }
+        );
     }
+}
 export default connect(mapStateToProps)(About);
