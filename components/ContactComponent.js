979c1b23e1bab4d2821ccd2141d85f16a573673f
Bonnie <mylilangls@gmail.com>
2020-09-24 08:46:25 -0700

    animatable animations

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,36 +1,27 @@
 import React, { Component } from "react";
 import { View, Text, ScrollView } from "react-native";
 import { Card } from "react-native-elements";
-import { CAMPSITES } from "../shared/campsites";
-import { PARTNERS } from "../shared/partners";
-import { PROMOTIONS } from "../shared/promotions";
+import * as Animatable from "react-native-animatable";
 
 class Contact extends Component {
-
-    static navigationOptions = {
-        title: "Contact Us",
-      };
-    render() {
-        return (
-            <ScrollView>
-                <Card title="Contact Us" wrapperStyle={{margin: 20}}>
-                    <Text>
-                        1 Nucamp Way
-                    </Text>
-                    <Text>Seattle, WA 98001</Text>
-                    <Text style={{marginBottom: 10}}>
-                        U.S.A.
-                    </Text>
-                    <Text>
-                        Phone: 1-206-555-1234
-                    </Text>
-                    <Text>Email: campsites@nucamp.co</Text>
-
-                </Card>
-
-            </ScrollView>
-        );
-    }
+  static navigationOptions = {
+    title: "Contact Us",
+  };
+  render() {
+    return (
+      <ScrollView>
+        <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+          <Card title="Contact Us" wrapperStyle={{ margin: 20 }}>
+            <Text>1 Nucamp Way</Text>
+            <Text>Seattle, WA 98001</Text>
+            <Text style={{ marginBottom: 10 }}>U.S.A.</Text>
+            <Text>Phone: 1-206-555-1234</Text>
+            <Text>Email: campsites@nucamp.co</Text>
+          </Card>
+        </Animatable.View>
+      </ScrollView>
+    );
+  }
 }
 
 export default Contact;
