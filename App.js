76eab2d1573e18a6ffdd93049f2cfb169f737c6b
Bonnie <mylilangls@gmail.com>
2020-09-25 13:08:44 -0700

    errors

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import Main from './components/MainComponent';
-import { Provider } from 'react-redux';
-import { ConfigureStore } from './redux/configureStore'
+import React from "react";
+import Main from "./components/MainComponent";
+import { Provider } from "react-redux";
+import { ConfigureStore } from "./redux/configureStore";
+import { PersistGate } from "redux-persist/es/integration/react";
+import Loading from "./components/LoadingComponent";
+import ErrorBoundary from "./components/ErrorComponent";
 
-const store = ConfigureStore();
+const { persistor, store } = ConfigureStore();
 
 export default function App() {
+  
   return (
     <Provider store={store}>
-      <Main />
+      <PersistGate 
+      loading={<Loading />} 
+      persistor={persistor}>
+        <Main />
+        <ErrorBoundary />
+      </PersistGate>
     </Provider>
-  );
+    
+  )
 }
